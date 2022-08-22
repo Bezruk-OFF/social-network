a0f3e71493b4db082e23867b1a48fa07ee01fd00
Ivan Bezrukov <van-84@yandex.ru>
2022-08-22 23:41:49 +0700

    93. Paginator

--- app/src/components/common/Paginator/Paginator.jsx
+++ app/src/components/common/Paginator/Paginator.jsx
@@ -3,21 +3,38 @@ import classes from './Paginator.module.css';
 
 let Paginator = (props) => {
 
+    let size = 7;
     let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
-        if (i == 25) {
-            break;
-        }
+        // if (i == 25) {
+        //     break;
+        // }
     }
 
+    let currentPages = pages;
+    let f = () => {
+        if (currentPages[currentPages.length - 1] != pages[pages.length - 1]) {
+            currentPages = pages.slice(currentPages.length);
+        }
+
+        currentPages.slice(size, 2 * size).map(p => {
+            return <span className={props.currentPage === p && classes.selectedPage}
+                onClick={() => { props.onPageChenged(p) }}>{p}</span>
+        })
+    }
+    currentPages = currentPages.slice(size);
+
+
     return <div>
-        {pages.map(p => {
+        <button className={classes.prev} type="submit" >PREV</button>
+        {pages.slice(0, size).map(p => {
             return <span className={props.currentPage === p && classes.selectedPage}  // Warning: Received `false` for a non-boolean attribute `className`.
                 onClick={() => { props.onPageChenged(p) }}>{p}</span>
         })}
+        <button className={classes.next} onclick={f}>NEXT</button>
     </div>
 }
 
